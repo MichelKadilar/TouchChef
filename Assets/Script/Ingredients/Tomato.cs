5b25b2998db0e443e70175fe8a4ae10687e3d7e1
Saad Beidouri <saad.beidouri@etu.unice.fr>
2024-11-01 17:43:24 +0100

    needed slices

--- Assets/Script/Ingredients/Tomato.cs
+++ Assets/Script/Ingredients/Tomato.cs
@@ -40,24 +40,34 @@ namespace Script.Ingredients
         // This method is called to slice the tomato
         public void Slice(Vector2 position, Camera camera, BaseIngredient ingredient)
         {
+            Debug.Log("WAAAAAAAAAA : Slicing tomato!");
             if (currentState == IngredientState.Raw)
             {
                 // Check if the slider has been instantiated
                 if (sliderInstance == null)
                 {
-                    Vector3 rawPosition = camera.ScreenToWorldPoint(new Vector3(position.x, position.y+3, 10));
-                    sliderInstance = Instantiate(sliderPrefab, rawPosition, Quaternion.identity, transform);
+                    // Position the slider in the middle of the screen (camera view center)
+                    
+                    Vector3 rawPosition = camera.ViewportToWorldPoint(new Vector3(ingredient.transform.position.x, ingredient.transform.position.y, 10));
+    
+                    // Instantiate the slider prefab at this position
+                    sliderInstance = Instantiate(sliderPrefab, rawPosition, Quaternion.identity);
+                    
+                    // Get the Slider component and set max and initial values
                     slider = sliderInstance.GetComponentInChildren<Slider>();
                     slider.maxValue = neededSlices;
                     slider.value = currentSlice;
                 }
 
+
                 
                 currentSlice++;
                 slider.value = currentSlice;
+                Debug.Log($"WAAAAAAAA : Sliced tomato! Current slice: {currentSlice}");
                 
                 if (currentSlice == neededSlices)
                 {
+                    Debug.Log("WAAAAAAAAAA : Sliced all slices!");
                     CompleteProcessing(ProcessType.Cut, position, camera, ingredient);
                 }
             }
@@ -65,13 +75,14 @@ namespace Script.Ingredients
 
         protected void CompleteProcessing(ProcessType processType, Vector2 position, Camera camera, BaseIngredient ingredient)
         {
+            Debug.Log("WAAAAAAAAAA : ANA WAST Completed processing!");
             switch (processType)
             {
                 case ProcessType.Cut:
                     currentState = IngredientState.Cut;
 
                     // Instantiate the cut visual when slicing is complete
-                    Vector3 rawPosition = camera.ScreenToWorldPoint(new Vector3(position.x, position.y, 15));
+                    Vector3 rawPosition = camera.ScreenToWorldPoint(new Vector3(position.x, position.y, 10));
                     instantiatedCutTomato = Instantiate(cutVisual, rawPosition, Quaternion.identity);
 
                     ingredient.gameObject.SetActive(false);
